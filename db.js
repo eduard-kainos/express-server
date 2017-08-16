879ea24c0e744446db1e6955053d2828d0f4e877
Eduard <eduardg@kainos.com>
2017-08-16 20:11:51 +0100

    Adds edit feature for city population.

--- db.js
+++ db.js
@@ -17,11 +17,25 @@ exports.getCitiesInCountry = function (countrycode, callback){
     db.query(
         "SELECT id, name, countrycode, district, population " +
         "FROM city " +
-        "WHERE CountryCode = ?",
+        "WHERE CountryCode = ? " +
+        "LIMIT 10",
         [countrycode],
         function(err, rows){
             if(err) throw err;
             callback(rows);
         }
     );
-}
+};
+
+exports.updateCityPop = function (cityID, cityPop, callback){
+    db.query(
+        "UPDATE city " +
+        "SET Population = ? " +
+        "WHERE ID = ?",
+        [cityPop, cityID],
+        function(err){
+            if(err) throw err;
+            callback("Update successful.");
+        }
+    );
+};
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const db = require('./db.js');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/cities', function(req, res){
     db.getCitiesInCountry('GBR', function(rows){
-        cities = rows;
+        res.send(rows);
+        console.log('Request processed: ');
     });
-    res.send(cities);
-    console.log('Request processed: ');
+});
+
+app.post('/city', function(req, res){
+    const cityID = req.body.cityID;
+    const cityPop = req.body.cityPop;
+
+    if(cityID && cityPop){
+        db.updateCityPop(cityID, cityPop, function(message){
+            res.send(message);
+        });
+    }
 });
 
 app.listen(8002, function() {
